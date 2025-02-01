9b837d46096f50a9d8992719337c05b31137132f
jaya verma <151820500+jayaverma21@users.noreply.github.com>
2025-02-01 21:06:44 +0530

    Add files via upload

---
+++ reportUsageOfAwsResources.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+###########################
+# Author : Jaya Verma
+# Date : 1 feb 25
+# Task : report the usage of AWS resources
+##################################
+
+set -x # debug mode
+
+# We Track :
+# AWS S3
+# AWS EC2
+# AWS Lembda
+# AWS IAM user
+
+#List s3 Buckets 
+echo "Print List of s3 buckets: "
+aws s3 ls 
+
+#List ec2 instances
+echo "Print List of EC2 instances: "
+aws ec2 describe-instances 
+
+#List lambda
+echo "Print List of lambda: "
+aws lambda lists-functions
+
+#List IAM users
+echo "Print List of IAM users: "
+aws iam list -users
+
